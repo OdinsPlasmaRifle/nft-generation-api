9c0f66717fc6cf72dc955bd548c322168c3ea72a
Connor Macdougall <cmacdougall78@gmail.com>
2021-10-22 18:26:47 +0200

    Added contract calling stuff

--- go.mod
+++ go.mod
@@ -5,4 +5,18 @@ go 1.17
 require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/justinas/alice v1.2.0
+	github.com/umbracle/go-web3 v0.0.0-20210921184341-1a00db77b7ed
+)
+
+require (
+	github.com/gorilla/websocket v1.4.1 // indirect
+	github.com/klauspost/compress v1.4.1 // indirect
+	github.com/klauspost/cpuid v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/umbracle/fastrlp v0.0.0-20210128110402-41364ca56ca8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.4.0 // indirect
+	github.com/valyala/fastjson v1.4.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 )
--- main.go
+++ main.go
@@ -11,13 +11,23 @@ import (
 	"image"
 	"image/draw"
 	"image/png"
+	"strconv"
 
-    "github.com/julienschmidt/httprouter"
+	"github.com/julienschmidt/httprouter"
+	
+	web3 "github.com/umbracle/go-web3"
+	"github.com/umbracle/go-web3/jsonrpc"
+	"github.com/umbracle/go-web3/abi"
+	"github.com/umbracle/go-web3/contract"
 )
 
 type Response struct {
 	HttpCode int
 	Data     interface{}
+}
+
+type ERC1155 struct {
+	c *contract.Contract
 }
 
 func (r *Response) RenderJson(w http.ResponseWriter) {
@@ -37,7 +47,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func tokenHandler(w http.ResponseWriter, r *http.Request) {
     q := r.URL.Query()
     address := q.Get("address")
-    id := q.Get("id")
+	id := q.Get("id")
+	check_id, err := strconv.Atoi(id)
 
 	res := Response{}
 
@@ -49,17 +60,48 @@ func tokenHandler(w http.ResponseWriter, r *http.Request) {
 		res.RenderJson(w)
 		return
 	}
+	
+	// Get contract details
+	var erc1155ABI = `[ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "approved", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "operator", "type": "address" }, { "indexed": false, "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "ApprovalForAll", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": true, "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "Transfer", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "approve", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "getApproved", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "getAttributes", "outputs": [ { "internalType": "string[]", "name": "", "type": "string[]" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "operator", "type": "address" } ], "name": "isApprovedForAll", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "ownerOf", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" }, { "internalType": "bytes", "name": "_data", "type": "bytes" } ], "name": "safeTransferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "operator", "type": "address" }, { "internalType": "bool", "name": "approved", "type": "bool" } ], "name": "setApprovalForAll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "bytes4", "name": "interfaceId", "type": "bytes4" } ], "name": "supportsInterface", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "index", "type": "uint256" } ], "name": "tokenByIndex", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "uint256", "name": "index", "type": "uint256" } ], "name": "tokenOfOwnerByIndex", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "tokenURI", "outputs": [ { "internalType": "string", "name": "", "type": "string" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "tokenId", "type": "uint256" } ], "name": "transferFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ]`
+	abiERC1155, err := abi.NewABI(erc1155ABI)
+	// Initiate the RPC web3 client
+	client, err := jsonrpc.NewClient("PUT_YOUR_INFURA_URL_HERE")
+	if err != nil {
+		panic(err)
+	}
+	// Get the latest block
+	number, err := client.Eth().BlockNumber()
+	if err != nil {
+		panic(err)
+	}
+	// Call the contract to get the attributes
+	var contract = ERC1155{c: contract.NewContract(web3.HexToAddress(address), abiERC1155, client)}
+	contract_output, err := contract.c.Call("getAttributes", web3.EncodeBlock(web3.BlockNumber(number)), check_id)
+
+	// fmt.Println(attributes["0"][0])
+	// s := strings.Split(attributes["0"].(string), " ")
+	attributes := contract_output["0"].([]string)
+	// fmt.Println(retval0[0])
 
 	// Get list of assets for the image.
 	// TODO : needs to figure these out based on the smart contract.
+	// paths := []string{
+	// 	"./assets/backgrounds/Bg-blue.png",
+	// 	"./assets/skin/Base-F-3.png",
+	// 	"./assets/outfits/Outfit2.png",
+	// 	"./assets/hair/Hair-blonde.png",
+	// 	"./assets/eyes/Eyes-blue.png",
+	// 	"./assets/lips/Lips-orange.png",
+	// 	"./assets/accessory/Acc-earring-gold.png",
+	// }
 	paths := []string{
-		"./assets/backgrounds/Bg-blue.png",
-		"./assets/skin/Base-F-1.png",
-		"./assets/outfits/Outfit1.png",
-		"./assets/hair/Hair-blonde.png",
-		"./assets/eyes/Eyes-blue.png",
-		"./assets/lips/Lips-orange.png",
-		"./assets/accessory/Acc-earring-gold.png",
+		fmt.Sprintf("./assets/backgrounds/Bg-%s.png", attributes[1]),
+		fmt.Sprintf("./assets/skin/Base-%s.png", attributes[6]),
+		fmt.Sprintf("./assets/outfits/Outfit%s.png", attributes[5]),
+		fmt.Sprintf("./assets/hair/Hair-%s.png", attributes[3]),
+		fmt.Sprintf("./assets/eyes/Eyes-%s.png", attributes[2]),
+		fmt.Sprintf("./assets/lips/Lips-%s.png", attributes[4]),
+		fmt.Sprintf("./assets/accessory/Acc-%s.png", attributes[0]),
 	}
 	var assets []image.Image
 	for _, path := range paths {
